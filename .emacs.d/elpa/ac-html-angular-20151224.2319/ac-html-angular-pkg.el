00db7a0600e4c0b797e0418bcf52e170c715d43d
Julien JEAN <julien.jean@epitech.eu>
2018-11-17 11:44:13 +0100

    rm elpa

--- .emacs.d/elpa/ac-html-angular-20151224.2319/ac-html-angular-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "ac-html-angular" "20151224.2319" "auto complete angular15 data for `ac-html' and `company-web'"
-  '((web-completion-data "0.1"))
-  :keywords
-  '("html" "auto-complete" "angular")
-  :url "https://github.com/osv/ac-html-bootstrap")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
